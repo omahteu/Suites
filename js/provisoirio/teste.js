a510ece7f06312b35aeb0f785df64c7fd9db9297
Matheus <matheus.mmn@hotmail.com>
2022-09-13 15:48:25 -0300

    teste de pin

--- js/provisoirio/teste.js
+++ js/provisoirio/teste.js
@@ -1,12 +1,3 @@
 $(document).on("click", "#teste_placa", function(){
-    //$("#testa_placa").removeAttr("style")
-    $.ajax({
-        url: "http://192.168.0.3/?L8",
-        success: function(data, textStatus, xhr) {
-            console.log(xhr);
-        },
-        complete: function(xhr, textStatus) {
-            console.log(xhr.status);
-        } 
-    });
+    $("#testa_placa").removeAttr("style")
 })
