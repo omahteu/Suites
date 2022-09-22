cafeeb36113b7963900912cd2320fd3212c72771
Matheus <matheus.mmn@hotmail.com>
2022-09-22 14:58:24 -0300

    tabela estoque

--- js/estoque/busca.js
+++ js/estoque/busca.js
@@ -1,4 +1,45 @@
 $("#BuscaInfoProduto").click(function(){
+
+    var form = document.forms.namedItem("formCadastroProdutos").id
+    console.log(form)
+
+    $("#formCadastroProdutos").append(
+        `<div class="control-group">`+
+            `<div class="controls">`+
+                `<h5 class="texto_produto" id="descricaoProduto"></h5>`+
+            `</div>`+
+        `</div>`+
+
+        `<div class="control-group">`+
+            `<div class="controls">`+
+                `<h5 class="texto_produto" id="valorUnitarioProduto"></h5>`+
+            `</div>`+
+        `</div>`+
+
+        `<div class="control-group">`+
+            `<div class="controls">`+
+                `<h5 class="texto_produto" id="categoriaProduto"></h5>`+
+            `</div>`+
+        `</div>`+
+
+        `<div class="control-group">`+
+            `<div class="controls">`+
+                `<select name="" id="acao_movimentacao">`+
+                    `<option value="" hidden>Ação</option>`+
+                    `<option value="entrada">Entrada</option>`+
+                    `<option value="saida">Saída</option>`+
+                `</select>`+
+            `</div>`+
+        `</div>`+
+
+        `<div class="control-group">`+
+            `<div class="controls">`+
+                `<input type="text" id="quantidadeProduto" placeholder="Quantidade">`+
+            `</div>`+
+        `</div>`
+    )
+
+
     let codigo_pesquisado = $("#codigoProduto").val()
     $.get("https://demomotelapi.herokuapp.com/produtos/", function(e){
         var dados = e.filter(el => el.codigo == codigo_pesquisado)
@@ -13,8 +54,10 @@ $("#BuscaInfoProduto").click(function(){
 
         });
     })
+    
     $(this).css("display", "none")
     $("#acao_movimentacao").css("display", "inline")
     $("#quantidadeProduto").css("display", "inline")
     $("#SalvarMovimentoEstoque").css("display", "inline")
+
 })
