a79103c47c6f72c25553c1a988ebc185830a2593
matheus <matheus.mmn@hotmail.com>
2022-09-26 06:39:08 -0300

    update php

--- php/updat/email.php
+++ php/updat/email.php
@@ -1,12 +1,12 @@
 <?php
-function update_igs($social, $fantasia, $cnpj, $cidade, $endereco, $numero, $bairro, $telefone, $telefone2, $telefone3, $id){
+function update_email($usuario, $senha, $smtp, $porta, $timeout, $email_destino, $email_contabilidade, $autenticacao, $id){
     include "../conexao.php";
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $sql = "UPDATE igs SET social='$social', fantasia='$fantasia', cnpj='$cnpj', cidade='$cidade', endereco='$endereco', numero='$numero', bairro='$bairro', telefone='$telefone', telefone2='$telefone2', telefone3='$telefone3' WHERE id='$id'";
+    $sql = "UPDATE email SET usuario='$usuario', senha='$senha', smtp='$smtp', porta='$porta', timeout='$timeout', email_destino='$email_destino', email_contabilidade='$email_contabilidade', autenticacao='$autenticacao' WHERE id='$id'";
 
     if ($conn->query($sql) === TRUE) {
         echo "Record updated successfully";
