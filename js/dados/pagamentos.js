855ac2d161b010f5286b7236c3272c694b44e681
Matheus <matheus.mmn@hotmail.com>
2022-09-30 13:46:44 -0300

    ajustes

--- js/dados/pagamentos.js
+++ js/dados/pagamentos.js
@@ -2,5 +2,13 @@ import { link } from "../setup/index.js"
 
 
 $(document).ready(function(){
-    
+    dados_pagamento()
 })
+
+async function dados_pagamento(){
+    const requisicao = await fetch(link[33])
+    const resposta = await requisicao.json()
+    resposta.forEach(e => {
+        console.log(e)
+    });
+}
