27d2f2c9b2e8e0f20378e0a16d456f65694be8db
matheus <matheus.mmn@hotmail.com>
2022-10-17 09:00:50 -0300

    ajuste de tempo

--- js/configuracoes/botoes/editar_tempos.js
+++ js/configuracoes/botoes/editar_tempos.js
@@ -1,21 +1,56 @@
 import { link } from "../../setup/index.js"
+import { abrirMenu } from "../exibicoes/menu.js"
 
-$(document).on("click", "#scet", function(){
+$(document).on("click", "#scet", function () {
+    abrirMenu("modau-menu")
+    var menu = document.forms.namedItem("menu").id
+    $(`#${menu}`).html(
+        '<button type="button" name="botao" class="btn btn-success" id="atualizar_tempo">Atualizar</button>'
+  
+    )
+    $(`#${menu}`).prepend(
+        '<select id="tip_tempo">'+
+            '<option hidden>Tempos</option>'+
+            '<option>Desistência</option>'+
+            '<option>Faxina</option>'+
+            '<option>Limpeza</option>'+
+            '<option>Manutenção</option>'+
+            '<option>Troca de Quarto</option>'+
+        '</select>'+
+        '<input type="text" id="novo_tempo" placeholder="Novo Tempo" required>'
+    )
+})
+
+
+/*
     var tempo = $("#escolhe_tempo").val()
-    //let valor = $(`#${tempo}`).val()
+    var valor = $(`#${tempo}`).val()
 
-    var dados = [tempo, "casa"]
-    var dds = JSON.stringify(dados)
+    var ficha = {
+        tempo: tempo,
+        valor: valor
+    }
+    
+    sessionStorage.setItem(tempo, JSON.stringify(ficha))
 
+    var cde = JSON.parse(sessionStorage.getItem(tempo))
+    var dsfg = cde["tempo"]
+
+    
 
     $.ajax({
-        url: link[19] + "1" + "/",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        url: `${link[19]}1/`,
         type: 'PATCH',
-        dataType: 'json',
-        data: dds,
-        success: function() {
-            alert("Valores Salvos!")
-            location.reload()
+        data: JSON.stringify({ dsfg: valor }),
+        success: function () {
+            console.log("Troca Com Sucesso!");
+        },
+        error: function (textStatus, errorThrown) {
+            console.log(`ERRO: ${textStatus} - ${errorThrown}`)
         }
     })
-})
+*/
--- js/configuracoes/tempo.js
+++ js/configuracoes/tempo.js
@@ -3,7 +3,21 @@ import { link } from "../setup/index.js"
 export async function seleciona_tempo() {
     const resposta = await fetch(link[19])
     const dados = await resposta.json()
+    var tabela = document.getElementById("tabelaTempos")
+    tabela.innerHTML = ""
     dados.forEach(e => {
+        tabela.innerHTML += '<tr>' +
+            `<td><h5>${e.desistencia}</h5></td>` +
+            `<td><h5>${e.faxina}</h5></td>` +
+            `<td><h5>${e.limpeza}</h5></td>` +
+            `<td><h5>${e.manutencao}</h5></td>` +
+            `<td><h5>${e.troca}</h5></td>` +
+            `<td><buttoon type="button" class="btn btn-primary" id="scet">Alterar</button></td>` +
+            '</tr>'
+    })
+}
+
+/*
         $("#escolhe_tempo").change(function () {
             var option = $(this).find(":selected").text()
             var card = document.forms.namedItem("formTempo")[1]
@@ -39,5 +53,16 @@ export async function seleciona_tempo() {
                 .removeAttr("style")
             }
         })
-    })
-}
+        */
+
+
+
+/*
+        console.log(e)
+        tabela.innerHTML += '<tr>'+
+                                `<td>${e.desistencia}</td>`+
+                                `<td>${e.faxina}</td>`+
+                                `<td>${e.limpeza}</td>`+
+                                `<td>${e.manutencao}</td>`+
+                                `<td>${e.troca}</td>`+
+                            '</tr>'*/
