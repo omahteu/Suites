b3b1f2c35749e9785a9aa7554952d223cd4aa369
Matheus <matheus.mmn@hotmail.com>
2022-10-28 14:43:54 -0300

    terminar pernoite

--- js/pernoite/tipoum.js
+++ js/pernoite/tipoum.js
@@ -1,16 +1,59 @@
 import { link } from "../setup/index.js"
+import { hora_atual_segundos } from "../geradores/hora.js"
+import { numero } from "../geradores/numero.js"
+import { pernoite } from "../tags/pernoite.js"
+import { modos } from "../setup/box.js"
+import { desfazer } from "../tags/desfazer.js"
 
 $(document).ready(function(){
     setInterval( () => {
-        async function pernoite(){
+        async function pernoitex(){
+            const tolerancia = JSON.parse(sessionStorage.getItem("dq"))
             const requisicao = await fetch(link[35])
             const retorno = await requisicao.json()
             if (retorno.length > 0){
                 retorno.forEach(e => {
-                    console.log(e)
-                });
+                    let tipo = e.tipo
+                    if (tipo == "Automática"){
+                        $.get(link[11], (el) => {
+                            el.forEach(ele => {
+                                let id = ele.id
+                                let suite = ele.quarto
+                                let hora = ele.datahora
+                                let valor = ele.valor
+
+                                var flags = modos.slice(0, 3)
+                                let tempoTolerancia = tolerancia.filter(i => i.numero == suite)
+                                let minutoTolerancia = tempoTolerancia[0].tolerancia
+                                let agora = hora_atual_segundos()
+                                let datahoraLocacao = ele.datahora
+                                var ms = moment(agora, "HH:mm:ss").diff(moment(datahoraLocacao, "HH:mm:ss"));
+                                var d = moment.duration(ms);
+                                var tempoPassado = Math.floor(d.asHours()) + moment.utc(ms).format(":mm:ss");
+                                if (Number(tempoPassado.charAt(0)) > 0){
+                                    pernoite(suite, "", flags[0], flags[1], flags[2])
+                                    var dados = {
+                                        datahora: hora,
+                                        valor: valor,
+                                        quarto: suite,
+                                        tipo: "pernoite"
+                                    }
+                                    $.ajax({
+                                        url: `${link[11]}${id}/`,
+                                        type: "PUT",
+                                        dataType: "json",
+                                        data: dados,
+                                        success:  () => {
+                                            console.log("Atualizado.")
+                                        }
+                                    })
+                                }
+                            })
+                        })
+                    }
+                })
             }
         }
-        // pernoite()
+        pernoitex()
     }, 1000)
 })
--- js/tags/pernoite.js
+++ js/tags/pernoite.js
@@ -1,42 +1,16 @@
 import { hora_atual } from "../geradores/hora.js"
 
 export function pernoite(q, t, x, y, z) {
-    $("#quarto" + q).css({
-        "background-color": "#8B008B",
-        "opacity": 0.5})
+    $(`.cardBox .card:nth-child(${q})`).css({
+        "background": "#8B008B"
+    })
+    $(`[name=${q}]`).css('display', 'none')
+    $(".acoes1").css('display', 'inline-block')
+    $(".acoes1").val('Encerrar')
     
-    $("#tempo").css('margin-top', '-30px')
-
-    $("[id=botaoq" + q + "]").css('visibility', 'hidden')
-
     var hora = hora_atual()
-    $("[id=tempo]").text(hora)
-
-    $("#" + x).css('visibility', 'visible')
-    $("#" + x).val('Alterar P/ Locação')
-
-    $("#" + y).css('visibility', 'visible')
-    $("#" + y).val('Encerrar')
-
-    if(t != 'btn pernoite'){
-        $("#" + z).css('visibility', 'hidden')
-        $("#" + z).val('Cancelar Reserva')
-    } 
-
-    var tipoQuarto = $('#tipo' + q).text()
-
-    if(tipoQuarto == "Ar"){
-        $("#valor-quarto").text('')
-    } else if(tipoQuarto == "Ventilador"){
-        $("#valor-quarto").text('')
-    }
-    $("#numquarto").text(q)
     $("#tipo").text('pernoite')
-    $("#intervalo").text(x + "," + y + "," + z)
-    $("#entrada").text(String(hora) + ':' + String(minutos) + 'h')
-    $("#imagemQuarto" + q).css('border', '2px solid rgb(139, 0, 139)')
-    $("#imagemQuarto" + q).css('box-shadow', 'inset 0 0 1em rgb(139, 0, 139), 0 0 1em #000')
+    $("#quarto_painel").text(q)
+    $("#intervalo").text(`${x},${y},${z}`)
+    $("#entrada").text(hora)
 }
- 
-// adicionar mais um parâmetro
-// 
