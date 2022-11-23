c782682b573e5f42d7f0da5c71f5efa89764b9a2
Matheus <matheus.mmn@hotmail.com>
2022-11-23 13:40:16 -0300

    ajustes na troca de quartos

--- js/setup/substituir.js
+++ js/setup/substituir.js
@@ -1,9 +1,6 @@
 import { link } from "../setup/index.js"
-import { inicia } from "../contadores/cronometros/_relogio1.js"
-
-import { _crnmtra2 } from "../contadores/restart/c2.js"
-import { parar } from "../contadores/cronometros/c1.js"
-import { parar2 } from "../contadores/cronometros/c2.js"
+import { inicia, para } from "../contadores/cronometros/_relogio1.js"
+import { inicia2, para2 } from "../contadores/cronometros/_relogio2.js"
 import { locado } from "../tags/locacao.js"
 import { fimModalTroca } from "../setup/troca.js"
 import { aguardando } from "../tags/aguardo.js"
@@ -12,86 +9,40 @@ import { registra_troca } from "../quartos/ajax/post/troca.js"
 import { tempo_pausado } from "../quartos/ajax/post/decorrido.js"
 import { tick } from "../setup/box.js"
 
-
-
-$("#substituir").click(function(){
-
-    var quarto = $("#quarto_antigo").val()
-    var novo = $("#quartos_disponiveis").val()
-    var usuario = $("#usuario_sistema").text()
+$("#substituir").click(function () {
+    let quarto = $("#quarto_antigo").val()
+    let novo = $("#quartos_disponiveis").val()
+    let usuario = $("#usuario_sistema").text()
     let hora = $(`#hora${quarto}`).text()
     let minuto = $(`#minuto${quarto}`).text()
     let segundo = $(`#segundo${quarto}`).text()
-
-    $.get(link[5], function(e){
-        var dados = e.filter(item => item.quarto === quarto)
-        dados.forEach(el => {
-            var id = el.id
-            $.ajax({
-                headers : {
-                    'Accept' : 'application/json',
-                    'Content-Type' : 'application/json'
-                },
-                url : link[5] + id + "/",
-                type : 'PATCH',
-                data : JSON.stringify({quarto: novo}),
-                success : function() {
-                    console.log("Troca Com Sucesso!");
-                },
-                error : function(textStatus, errorThrown) {
-                    console.log(`ERRO: ${textStatus} - ${errorThrown}`)
-                }
-            })    
-        })
-    })
-    $.get(link[15], function(e){
-        var dados = e.filter(item => item.quarto === quarto)
-        dados.forEach(e => {
-            var id = e.id
-            $.ajax({
-                headers : {
-                    'Accept' : 'application/json',
-                    'Content-Type' : 'application/json'
-                },
-                url : link[15] + id + "/",
-                type : 'PATCH',
-                data : JSON.stringify({quarto: novo}),
-                success : function() {
-                    console.log("Troca Com Sucesso!");
-                },
-                error : function(textStatus, errorThrown) {
-                    console.log(`ERRO: ${textStatus} - ${errorThrown}`)
-                }
-            })
-        })
-    })
-    setTimeout( () => {registra_troca(usuario, quarto, novo)}, 100)
-    setTimeout( () => {tempo_pausado(hora, minuto, segundo, quarto)}, 100)
-    setTimeout( () => {iniciando(novo)}, 1000)
-    setTimeout( () => {finalizando(quarto)}, 1500)
-    
+    trocaComanda(quarto, novo)
+    trocaPatio(quarto, novo)
+    setTimeout(() => { registra_troca(usuario, quarto, novo) }, 100)
+    setTimeout(() => { tempo_pausado(hora, minuto, segundo, quarto) }, 100)
+    setTimeout(() => { iniciando(quarto, novo, hora, minuto, segundo) }, 1000)
+    setTimeout(() => { finalizando(quarto) }, 1500)
 })
 
-function iniciando(suite){
-    var quarto = $("#quarto_antigo").val()
-    var hora = $(`#hora${quarto}`).text()
-    var minutos = $(`#minuto${quarto}`).text()
-    var segundos = $(`#segundo${quarto}`).text()
-    var permanencia = [hora, minutos, segundos]
+function iniciando(antigo, suite, hora, minuto, segundo) {
+    var p = [hora, minuto, segundo]
     let flags = tick[`${suite}`]
-    setTimeout( () => {locado(suite, flags[0], flags[1], flags[2])}, 200)
-    setTimeout( () => {suite == "1" ? inicia(suite, permanencia[0], permanencia[1], permanencia[2]) : _crnmtra2(suite, permanencia[0], permanencia[1], permanencia[2])}, 300)
-    setTimeout( () => {fimModalTroca()}, 400)
-    setTimeout( () => {
-        $.get(link[11], function(e){
-            var dados = e.filter(item => item.quarto === quarto)
+    setTimeout(() => { locado(suite, flags[0], flags[1], flags[2]) }, 200)
+    setTimeout(() => {
+        suite == "1" ? inicia(suite, p[0], p[1], p[2]) :
+        suite == "2" ? inicia2(suite, p[0], p[1], p[2]) : ""
+    }, 300)
+    setTimeout(() => { fimModalTroca() }, 400)
+    setTimeout(() => {
+        $.get(link[11], e => {
+            var dados = e.filter(item => item.quarto === antigo)
             dados.forEach(e => {
                 sessionStorage.setItem("ficha", JSON.stringify(e))
             })
         })
 
     }, 500)
-    setTimeout( () => {
+    setTimeout(() => {
         var card = JSON.parse(sessionStorage.getItem("ficha"))
         var ficha = {
             datahora: card.datahora,
@@ -99,41 +50,89 @@ function iniciando(suite){
             quarto: suite,
             tipo: "locado"
         }
-        $.post(link[11], ficha, function(){})
+        $.post(link[11], ficha, () => { sessionStorage.removeItem("ficha") })
     }, 700)
 }
 
-function finalizando(suite){
+function finalizando(suite) {
     let flags = tick[`${suite}`]
-    suite == "1" ? parar() : parar2()
-    setTimeout( () => {desfazer(suite, flags[0], flags[1], flags[2])}, 200)
-    setTimeout( () => {aguardando(suite, flags[0], flags[1], flags[2])}, 300)
-    setTimeout( () => {
-        $.get(link[11], function(e){
+    suite == "1" ? para() :
+        suite == "2" ? para2 : ""
+    setTimeout(() => { desfazer(suite, flags[0], flags[1], flags[2]) }, 200)
+    setTimeout(() => { aguardando(suite, flags[0], flags[1], flags[2]) }, 300)
+    setTimeout(() => {
+        $.get(link[11], function (e) {
             var dados = e.filter(item => item.quarto === suite)
             dados.forEach(e => {
                 sessionStorage.setItem("fichas", JSON.stringify(e))
             })
         })
-
     }, 500)
-    setTimeout( () => {
+    setTimeout(() => {
         var card = JSON.parse(sessionStorage.getItem("fichas"))
         var id = card.id
         $.ajax({
-            headers : {
-                'Accept' : 'application/json',
-                'Content-Type' : 'application/json'
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
             },
-            url : link[11] + id + "/",
-            type : 'PATCH',
-            data : JSON.stringify({tipo: "aguardando"}),
-            success : function() {
-                console.log("Troca Com Sucesso!");
+            url: `${link[11]}${id}/`,
+            type: 'PATCH',
+            data: JSON.stringify({ tipo: "aguardando" }),
+            success: function () {
+                sessionStorage.removeItem("fichas");
             },
-            error : function(textStatus, errorThrown) {
+            error: function (textStatus, errorThrown) {
                 console.log(`ERRO: ${textStatus} - ${errorThrown}`)
             }
         })
     }, 700)
+}
+
+function trocaComanda(antigo, novo) {
+    $.get(link[5], e => {
+        let dados = e.filter(item => item.quarto === antigo)
+        dados.forEach(el => {
+            var id = el.id
+            $.ajax({
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                url: `${link[5]}${id}/`,
+                type: 'PATCH',
+                data: JSON.stringify({ quarto: novo }),
+                success: function () {
+                    console.log("Troca de Comanda!");
+                },
+                error: function (textStatus, errorThrown) {
+                    console.log(`ERRO: ${textStatus} - ${errorThrown}`)
+                }
+            })
+        })
+    })
+}
+
+function trocaPatio(antigo, novo) {
+    $.get(link[15], e => {
+        let dados = e.filter(item => item.quarto === antigo)
+        dados.forEach(e => {
+            var id = e.id
+            $.ajax({
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                url: `${link[15]}${id}/`,
+                type: 'PATCH',
+                data: JSON.stringify({ quarto: novo }),
+                success: function () {
+                    console.log("Troca de Pátio!");
+                },
+                error: function (textStatus, errorThrown) {
+                    console.log(`ERRO: ${textStatus} - ${errorThrown}`)
+                }
+            })
+        })
+    })
 }
