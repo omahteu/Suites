ae79ad47795f055236a51d06c2a94f9a77d40ad8
Matheus <matheus.mmn@hotmail.com>
2022-11-23 16:04:31 -0300

    ajustes

--- js/setup/block.js
+++ js/setup/block.js
@@ -2,11 +2,15 @@ import { minuto_para_segundo } from "../conversores/minutos_milisegundos.js"
 import { link } from "../setup/index.js"
 
 $(".locado").click(function() {
+    let suite = $(this).attr("name")
+    trava(suite)
     $.get(link[19], e => {
         let tempo_troca = parseInt(e[0].troca)
         var millisegundos_troca = minuto_para_segundo(tempo_troca)
-        setTimeout( () => {tempo_para_troca()}, millisegundos_troca)
-    })
+        setTimeout( () => {
+            tempo_para_troca()
+        }, millisegundos_troca)
+    }) 
 })
 
 function tempo_para_troca(){
@@ -15,3 +19,21 @@ function tempo_para_troca(){
     $(".acoes2").css('visibility', 'visible')
     $(".acoes2").val('Encerrar')
 }
+
+function trava(suite) {
+    let ficha = {
+        suite: suite,
+        modo: "travado",
+        tipo: "",
+        horario: ""
+    }
+    $.post(link[34], ficha, () => {})
+}
+$(document).ready(function(){
+    let ficha = {
+        suite: 'suite',
+        valor: "travado",
+        tipo: "",
+    }
+    $.post(link[36], ficha, () => {})
+})
