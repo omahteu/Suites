0603d111c12a5f54322c6d465241c591b8d78543
matheus <matheus.mmn@hotmail.com>
2022-11-29 01:40:51 -0300

    trava adicionada

--- js/setup/block.js
+++ js/setup/block.js
@@ -3,12 +3,12 @@ import { link } from "../setup/index.js"
 
 $(".locado").click(function() {
     let suite = $(this).attr("name")
-    trava(suite)
     $.get(link[19], e => {
         let tempo_troca = parseInt(e[0].troca)
         var millisegundos_troca = minuto_para_segundo(tempo_troca)
         setTimeout( () => {
             tempo_para_troca()
+            trava(suite)
         }, millisegundos_troca)
     }) 
 })
@@ -23,17 +23,9 @@ function tempo_para_troca(){
 function trava(suite) {
     let ficha = {
         suite: suite,
-        modo: "travado",
+        modo: "tW",
         tipo: "",
         horario: ""
     }
-    $.post(link[34], ficha, () => {})
+    $.post(link[34], ficha, () => {console.log(`Travada possibilidade de troca na Suíte ${suite}!`)})
 }
-$(document).ready(function(){
-    let ficha = {
-        suite: 'suite',
-        valor: "travado",
-        tipo: "",
-    }
-    $.post(link[36], ficha, () => {})
-})
