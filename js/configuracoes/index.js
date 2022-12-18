062f5d39399bf1b41a46b5176be26530359b07bb
matheus <matheus.mmn@hotmail.com>
2022-12-18 11:36:21 -0300

    implementação do teto do caixa

--- js/configuracoes/index.js
+++ js/configuracoes/index.js
@@ -5,6 +5,7 @@ import { exibir_placas_cadastradas } from "./exibicoes/placas.js"
 import { exibir_quartos_cadastrados } from "./exibicoes/quartos.js"
 import { exibir_automacoes_cadastradas } from "./exibicoes/automacoes.js"
 import { exibir_pernoite } from "./exibicoes/pernoite.js"
+import { teto } from "./exibicoes/teto.js"
  
 $(document).ready(function() {
     seleciona_tempo()
@@ -15,4 +16,5 @@ $(document).ready(function() {
     exibir_quartos_cadastrados()
     exibir_automacoes_cadastradas()
     exibir_pernoite()
+    teto()
 })
