833a0c5678ec82ada6fb0c3adbf0fbb32719d5d2
matheus <matheus.mmn@hotmail.com>
2023-01-09 17:49:22 -0300

    espelhamento

--- espelhamento/aut.py
+++ espelhamento/aut.py
@@ -1,6 +1,20 @@
-from pyautogui import press
+from pyautogui import press, keyDown, keyUp
+from time import sleep
 
 
-def arg():
-    print("win")
-    press("win")
+def arg(driver):
+    keyDown("win")
+    sleep(1)
+    press("right")
+    sleep(1)
+    press("right")
+    sleep(1)
+    press("right")
+    sleep(1)
+    press("right")
+    sleep(1)
+    press("up")
+    sleep(1)
+    keyUp("win")
+    sleep(1)
+    driver.quit()
--- espelhamento/main.py
+++ espelhamento/main.py
@@ -7,6 +7,7 @@ from time import sleep
 
 # SETUP
 url = 'http://127.0.0.1:5501/html/checkout.html'
+nurl = 'http://127.0.0.1:5501/html/nota.html'
 option = Options()
 option.headless = True
 driver = webdriver.Chrome()
@@ -16,7 +17,12 @@ btn = driver.find_element(By.XPATH, '//*[@id="espelho"]')
 sleep(5)
 
 if btn.get_attribute("data-toggle") == "on":
+    sleep(1)
+    driver.execute_script("window.open('', '_blank');") 
+    driver.switch_to.window(driver.window_handles[1]) 
+    driver.get(nurl)
+    sleep(1)
     from aut import arg
-    arg()
+    arg(driver)
 
-sleep(500)
+sleep(1000)
