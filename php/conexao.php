7cba98348138085cbc8cb61f6efd8377db310241
matheus <matheus.mmn@hotmail.com>
2023-01-12 12:43:45 -0300

    nao tem php

--- php/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$servidor = "us125-cp.valueserver.com.br";
-	$usuario = "fortal_adm_garantias";
-	$senha = "{xwl&EGy]o(#";
-	$dbname = "fortal_garantias";
-    $port = 3306;
-    $conn = new mysqli($servidor, $usuario, $senha, $dbname);
-?>
